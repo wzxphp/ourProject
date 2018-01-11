5077eb954fbcd22b9900b5f2c9f843c9983e3dd4
wzxphp <wangzhixing1123@163.com>
2018-01-11 19:41:19 +0800

    前台首页商品页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hey! Friends!
                 </div>
 
                 <div class="links">
