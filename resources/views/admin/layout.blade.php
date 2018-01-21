467adf958cfc83580ee24d071a09547d52f7981e
wzxphp <wangzhixing1123@163.com>
2018-01-21 20:55:52 +0800

    后台合并分类

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -65,6 +65,48 @@
                         </li>
                     </ul>
                 </li>
+                <li class="list" >
+                    <a href="javascript:;">
+                        <i class="iconfont">&#xe6a3;</i>
+                        角色管理
+                        <i class="iconfont nav_right">&#xe697;</i>
+                    </a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="{{ url('admin/role') }}">
+                                <i class="iconfont">&#xe6a7;</i>
+                                角色列表
+                            </a>
+                        <li>
+                            <a href="{{ url('admin/role/create') }}">
+                                <i class="iconfont">&#xe6a7;</i>
+                                角色添加
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="list" >
+                    <a href="javascript:;">
+                        <i class="iconfont">&#xe6a3;</i>
+                        权限管理
+                        <i class="iconfont nav_right">&#xe697;</i>
+                    </a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="{{ url('admin/permission') }}">
+                                <i class="iconfont">&#xe6a7;</i>
+                                权限列表
+                            </a>
+
+                        </li>
+                        <li>
+                            <a href="{{ url('admin/permission/create') }}">
+                                <i class="iconfont">&#xe6a7;</i>
+                                权限添加
+                            </a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="list">
                     <a href="javascript:;">
                         <i class="iconfont">&#xe70b;</i>
@@ -86,24 +128,13 @@
                         </li>
                     </ul>
                 </li>
-                <li class="list" >
+                <li class="list">
                     <a href="javascript:;">
-                        <i class="iconfont">&#xe6a3;</i>
-                        角色管理
+                        <i class="iconfont">&#xe70b;</i>
+                        分类管理
                         <i class="iconfont nav_right">&#xe697;</i>
                     </a>
                     <ul class="sub-menu">
-                        <li>
-                            <a href="{{ url('admin/role') }}">
-                                <i class="iconfont">&#xe6a7;</i>
-                                角色列表
-                            </a>
-                        <li>
-                            <a href="{{ url('admin/role/create') }}">
-                                <i class="iconfont">&#xe6a7;</i>
-                                角色添加
-                            </a>
-                        </li>
                         <li>
                             <a href="{{ url('admin/cate/create') }}">
                                 <i class="iconfont">&#xe6a7;</i>
@@ -129,44 +160,6 @@
                             <a href="{{ url('admin/order/index') }}">
                                 <i class="iconfont">&#xe6a7;</i>
                                 订单列表
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="list" >
-                    <a href="javascript:;">
-                        <i class="iconfont">&#xe6a3;</i>
-                        轮播管理
-                        <i class="iconfont nav_right">&#xe697;</i>
-                    </a>
-                    <ul class="sub-menu" style="display:none">
-                        <li>
-                            <a href="./banner-list.html">
-                                <i class="iconfont">&#xe6a7;</i>
-                                轮播列表
-                            </a>
-
-                        </li>
-                    </ul>
-                </li>
-                <li class="list" >
-                    <a href="javascript:;">
-                        <i class="iconfont">&#xe6a3;</i>
-                        权限管理
-                        <i class="iconfont nav_right">&#xe697;</i>
-                    </a>
-                    <ul class="sub-menu">
-                        <li>
-                            <a href="{{ url('admin/permission') }}">
-                                <i class="iconfont">&#xe6a7;</i>
-                                权限列表
-                            </a>
-
-                        </li>
-                        <li>
-                            <a href="{{ url('admin/permission/create') }}">
-                                <i class="iconfont">&#xe6a7;</i>
-                                权限添加
                             </a>
                         </li>
                     </ul>
